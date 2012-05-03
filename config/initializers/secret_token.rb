09eff1fc7e50b1e91def1ee8d27c1127306a983b
blablo <blablo@gmail.com>
2012-05-03 17:08:28 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comidero::Application.config.secret_token = 'bf19570bfe19273b3fb0be16d273cb914c5dff57229cdc7f20c8dfac21dd3675c10bbfad8e8adefa1c01a1baccc4b68cb1bc1245789b0062d84073ff3b2cc457'
