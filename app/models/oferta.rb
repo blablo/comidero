9d1fd145284f57f6b83c7c16d6aa2994bba84908
blablo <blablo@gmail.com>
2012-06-11 17:01:16 -0500

    ofertas desc

--- app/models/oferta.rb
+++ app/models/oferta.rb
@@ -12,6 +12,6 @@ validates_attachment_presence :picture
 validates_attachment_content_type :picture, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] 
 #  :content_type => { :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] }
 
-  scope :by_wday, lambda {|wday| where("ofertas.days like '%?%'", wday) unless wday.nil?}
+  scope :by_wday, lambda {|wday| where("ofertas.days like '%?%'", wday).order("created_at DESC") unless wday.nil?}
 
 end
