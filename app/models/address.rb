27e60e6b18303ff5e476589a1f823eabbbbe6c0b
blablo <blablo@gmail.com>
2012-06-12 17:24:05 -0500

    geocode

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
+# -*- coding: undecided -*-
 class Address < ActiveRecord::Base
   belongs_to :oferta
+  geocoded_by :full_street_address   # can also be an IP address
+  after_validation :geocode        
+
+  def full_street_address
+    "México, Jalisco, Guadalajara, " + self.street
+  end
+
 end
